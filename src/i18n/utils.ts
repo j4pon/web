cd80f4896114ab42e38d7dab759bec00861487e4
japon27 <j4pon@hotmail.com>
2024-06-20 03:49:02 -0500

    test x 7

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,9 +1,25 @@
 import { ui, defaultLang, showDefaultLang } from './ui';
 
+/*
 export function getLangFromUrl(url: URL) {
   const [, lang] = url.pathname.split('/');
   if (lang in ui) return lang as keyof typeof ui;
   return defaultLang;
+}
+*/
+
+export function getLangFromUrl(url: URL) {
+  // Divide el pathname en partes
+  const parts = url.pathname.split('/').filter(part => part);
+
+  // Verifica si la segunda parte (index 1) o la tercera parte (index 2) es un idioma válido
+  const lang = parts.length > 1 && parts[1] in ui
+    ? parts[1]
+    : parts.length > 2 && parts[2] in ui
+    ? parts[2]
+    : defaultLang;
+
+  return lang as keyof typeof ui;
 }
 
 export function useTranslations(lang: keyof typeof ui) {
