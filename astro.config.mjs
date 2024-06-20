c60d134da69db576003c190c3d315fee86f42c77
japon27 <j4pon@hotmail.com>
2024-06-20 02:37:09 -0500

    test x 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://j4pon.github.io',
-  base: '/web',
+  base: 'web',
   integrations: [tailwind(), react({
     include: ['**/react/*']
   })],
